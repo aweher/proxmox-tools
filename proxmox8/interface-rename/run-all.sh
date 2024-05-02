ff1b6bbdf57b54d2b3a035bb0a32fb05094f54c8
Ariel Weher <ariel@weher.net>
2024-05-02 01:57:05 -0300

    Update wget commands in run-all.sh script to use correct output file names

--- proxmox8/interface-rename/run-all.sh
+++ proxmox8/interface-rename/run-all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 clear
-wget -q https://raw.githubusercontent.com/aweher/proxmox-tools/main/proxmox8/interface-rename/interface-rename.sh -o /tmp/rename-interfaces.sh
-wget -q https://raw.githubusercontent.com/aweher/proxmox-tools/main/proxmox8/interface-rename/update-interfaces.sh -o /tmp/update-interfaces.sh
+wget -q https://raw.githubusercontent.com/aweher/proxmox-tools/main/proxmox8/interface-rename/interface-rename.sh -O /tmp/rename-interfaces.sh
+wget -q https://raw.githubusercontent.com/aweher/proxmox-tools/main/proxmox8/interface-rename/update-interfaces.sh -O /tmp/update-interfaces.sh
 echo "This script will modify your network interfaces."
 echo "Press CTRL+C to exit, any other key to continue..."
 read onekey
 cd /tmp
-bash rename-interfaces.sh && bash update-interfaces.sh
+bash ./rename-interfaces.sh && bash ./update-interfaces.sh
 echo "Done. Changes will take effect after reboot."
