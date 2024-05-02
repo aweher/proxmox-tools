fd4495200b4c1afb7f7e9826644f21e3a32f2c19
Ariel Weher <ariel@weher.net>
2024-05-02 01:49:32 -0300

    Refactor run-all.sh script to use 'read' instead of 'pause' for user input

--- proxmox8/interface-rename/run-all.sh
+++ proxmox8/interface-rename/run-all.sh
@@ -4,7 +4,7 @@ wget -q https://raw.githubusercontent.com/aweher/proxmox-tools/main/proxmox8/int
 wget -q https://raw.githubusercontent.com/aweher/proxmox-tools/main/proxmox8/interface-rename/update-interfaces.sh -o /tmp/update-interfaces.sh
 echo "This script will modify your network interfaces."
 echo "Press CTRL+C to exit, any other key to continue..."
-pause
+read k
 cd /tmp
 bash rename-interfaces.sh && bash update-interfaces.sh
 echo "Done. Changes will take effect after reboot."
